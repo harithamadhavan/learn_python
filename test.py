7f783522a8cb4ef008660e14343108bfec497b36
Haritha Madhavan <harittamadhav@gmail.com>
2025-02-12 22:11:07 +0530

    added new scripts

---
+++ test.py
@@ -0,0 +1,9 @@
+# Input list of numbers
+numbers = [23, 1, 45, 34, 78, 2, 99, 56]
+
+# Find largest and smallest numbers
+largest = max(numbers)
+smallest = min(numbers)
+
+print(f"Largest number: {largest}")
+print(f"Smallest number: {smallest}")
