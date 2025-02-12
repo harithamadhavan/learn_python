67b6cf73663efffab56b6611602bbb60ca7ddc3a
harithamadhavan <87533966+harithamadhavan@users.noreply.github.com>
2025-02-12 22:01:24 +0530

    Updated data.py by diff user

--- data.py
+++ data.py
@@ -2,7 +2,15 @@
 num = int(input("Enter a number: "))
 
 # Check if the number is even or odd
-if num % 2 == 0:
+if num % 2 == 0:# Get user input
+text = input("Enter a string: ")
+
+# Check if the string is a palindrome
+if text == text[::-1]:
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
+
     print(f"{num} is Even.")
 else:
     print(f"{num} is Odd.")
